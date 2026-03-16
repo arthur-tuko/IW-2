33d6f84001ff6efdcbdeff98bc33651d48048a80
arthur-tuko <arthurfranco502@gmail.com>
2026-03-16 08:44:39 -0300

    Add files via upload

---
+++ pc dos d/abu.js
@@ -0,0 +1,50 @@
+Number(num = prompt("Digite um número:  "))
+
+if (num%2==0) {
+    alert("par")
+} else {
+    alert("ímpar")
+}
+
+Number(idade = prompt("Qual sua idade: "))
+
+if (idade >=18){
+    alert("Maior de idade")
+} else{
+    alert("menor de idade")
+}
+
+i = 0
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    
+}
+console.log("=============================================")
+o = 10
+
+while (o != 0){
+    o--
+    console.log(o)
+}
+console.log("==============================================")
+
+
+Number(numero2 = prompt("Digita um numero ai fiot: "))
+for(i=0; i <11; i++){
+    console.log(numero2*i)
+}
+
+console.log("------------------------------------------")
+
+soma = Number(prompt("Digite um número para a soma:  "))
+multi=0
+for (o=0; o<=soma; o++){
+    multi+=o
+    console.log(multi)
+}
+
+rillo = Number(prompt("Digite um número:  "))
+if (rillo = rillo /1) {
+    
+}
