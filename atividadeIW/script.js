048387e8a381eab6c40c021279ed969619363e1c
arthur-tuko <arthurfranco502@gmail.com>
2026-04-27 10:45:44 -0300

    Add files via upload

---
+++ atividadeIW/script.js
@@ -0,0 +1,8 @@
+function alterar(){
+    txt = String(document.getElementById('txt').value)
+    txtalt = document.getElementById('txtalt')
+
+
+    document.getElementById("txtalt").innerText=txt
+
+}
